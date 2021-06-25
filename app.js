17231d142fd9a332e053bfbcd12b58ea39520ca3
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-25 12:42:03 +0530

    db connection

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+//requiressss
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config({path: "./config.env"});
+
+//database
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connection Successful!"));
+//apppp
 const app = new express();
 app.use(express.static("public"));
 
