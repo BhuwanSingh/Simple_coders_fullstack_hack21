71f7028e3e2743fb3c51d581a5168ef03d610e29
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-25 12:44:06 +0530

    main page added

--- app.js
+++ app.js
@@ -19,13 +19,15 @@ mongoose
         useUnifiedTopology: true,
     })
     .then(() => console.log("DB Connection Successful!"));
-//apppp
+    
 const app = new express();
 app.use(express.static("public"));
 
 const ejs = require("ejs");
 app.set("view engine" , "ejs");
 
+const mainController = require("./controllers/mainPage");
+app.get("/", mainController);
 
 app.listen( process.env.PORT || 3000, ()=>{
     console.log("server Running");
