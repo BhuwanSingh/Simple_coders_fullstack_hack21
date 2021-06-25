6e30774bcb5d9db0be4ffb042e1f5a61966b4404
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-25 15:16:47 +0530

    sdfd

--- controllers/sign_upPage.js
+++ controllers/sign_upPage.js
@@ -1,10 +1,5 @@
 module.exports = (req, res) => {
-<<<<<<< HEAD
   res.render("sign_up", {
     error: flash("validaionError"),
   });
 };
-=======
-  res.render('sign_up')
-}
->>>>>>> faf3da4c8ff8ecf47337ff9378ad3ab06855f11e
