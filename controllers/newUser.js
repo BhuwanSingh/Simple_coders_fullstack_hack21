ec8e46d4541c6caf6fe534847b2be1064a073cf7
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-25 14:56:45 +0530

    user signup verification

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 
 module.exports = (req , res) => {
-    User.create({ ...req.body ,  }, (error,user) =>{
+    User.create({ ...req.body , secret_code : Math.floor(Math.randon() * 100000) }, (error,user) =>{
         if(error){
             const vaidationErrors = Object.keys(error.errors).map(key => errors.error[key].message);
             req.flash("data" , req.body )
