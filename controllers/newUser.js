1aa0f9902f1d5875e731c24639ee4dbfabe64623
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-25 15:14:25 +0530

    Update newUser.js

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,12 +1,20 @@
-const User = require("../models/User");
+const User = require('../models/User')
 
-module.exports = (req , res) => {
-    User.create({ ...req.body , secret_code : Math.floor(Math.randon() * 100000) }, (error,user) =>{
-        if(error){
-            const vaidationErrors = Object.keys(error.errors).map(key => errors.error[key].message);
-            req.flash("data" , req.body )
-            return res.redirect("/sign_up");
-        } 
-        res.redirect("/")
-    })
+module.exports = (req, res) => {
+  User.create(
+    { ...req.body, secret_code: Math.floor(Math.randon() * 100000) },
+    (error, user) => {
+      if (error) {
+        const vaidationErrors = Object.keys(error.errors).map(
+          (key) => errors.error[key].message
+        )
+        req.flash('data', req.body)
+        return res.redirect('/sign_up')
+      }
+      if (req.body.age >= 70) {
+        return res.redirect('/van_system')
+      }
+      res.redirect('/registration')
+    }
+  )
 }
