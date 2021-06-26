4c14b5c4981fa8d681396c982deb30ef687565f9
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-26 18:34:40 +0530

    schema update

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -5,6 +5,10 @@ module.exports = async (req, res) => {
   console.log(req.session.userId)
   const user = await User.findById(req.session.userId)
   const center = await Center.findById(req.body.slots)
+
+
+
   
+  const userUpdated = await User.findByIdAndUpdate(req.session.userId, )
   res.render('user_profile', { user })
 }
