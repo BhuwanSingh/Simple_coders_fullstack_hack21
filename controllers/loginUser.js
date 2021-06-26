868fe1a9042846489dd7d1fc5abca0dd9532411f
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-26 16:08:06 +0530

    res.redirect

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,18 +1,18 @@
-const User = require("../models/User");
+const User = require('../models/User')
 module.exports = (req, res) => {
-  const { aadhar, secret_code } = req.body;
+  const { aadhar, secret_code } = req.body
   User.findOne({ aadhar }, (error, user) => {
-    console.log(user.secret_code);
-    console.log(secret_code);
+    console.log(user.secret_code)
+    console.log(secret_code)
     if (user) {
       if (user.secret_code == secret_code) {
-        req.session.userID = user._id;
-        console.log("User logged in:", aadhar);
+        req.session.userID = user._id
+        console.log('User logged in:', aadhar)
         // res.redirect("/user/profile");
-        res.render("user_profile", { user });
+        res.render('user_profile', { user })
       }
     } else {
-      redirect("/login");
+      res.redirect('/login')
     }
-  });
-};
+  })
+}
