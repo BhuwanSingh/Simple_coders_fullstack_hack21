a6c777f43c8904d5ab3bf4a3debd60f97e8e782d
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-26 19:27:11 +0530

    update tokens and bookings

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -7,20 +7,22 @@ module.exports = async (req, res) => {
   const user = await User.findById(req.session.userId)
   const center = await Center.findById(req.body.slots)
 
-  const num = await Book.countDocuments({})
+  const num = await Book.countDocuments({ center_id: center._id })
   console.log(num)
 
   const book = await Book.create({
     aadhar: user.aadhar,
     age: user.age,
     city: user.city,
+    user_id: user._id,
     pwd: user.pwd,
     secret_code: user.secret_code,
     dose: user.dose + 1,
+    center_id: center._id,
     center_name: center.name,
     center_address: center.address,
-    token: 1,
+    token: num + 1,
   })
 
-  res.render('user_profile', { user })
+  res.render('user_profile', { book })
 }
--- models/book.js
+++ models/book.js
@@ -1,22 +1,23 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const booking = new Schema({
   aadhar: String,
   age: Number,
   city: String,
   pwd: Boolean,
+  user_id: Object,
   secret_code: String,
   dose: {
     type: Number,
     enum: [0, 1, 2],
     default: 0,
   },
+  center_id: Object,
   center_name: String,
   center_address: String,
   token: Number,
-});
+})
 
-
-const Book = mongoose.model("Book", booking);
-module.exports = Book;
+const Book = mongoose.model('Book', booking)
+module.exports = Book
