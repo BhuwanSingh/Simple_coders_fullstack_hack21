aec6200c9b517e7f9551fc29496522d477c52490
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-26 18:46:45 +0530

    local trash

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -1,14 +1,19 @@
-const User = require('./../models/User')
-const Center = require('./../models/center')
+const User = require("./../models/User");
+const Center = require("./../models/center");
 
 module.exports = async (req, res) => {
-  console.log(req.session.userId)
-  const user = await User.findById(req.session.userId)
-  const center = await Center.findById(req.body.slots)
+  console.log(req.session.userId);
+  const user = await User.findById(req.session.userId);
+  const center = await Center.findById(req.body.slots);
 
+  cost disUser.findByIdAndUpdate(user_id, { name: "Gourav" }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Updated User : ", docs);
+    }
+  });
 
-
-  
-  const userUpdated = await User.findByIdAndUpdate(req.session.userId, )
-  res.render('user_profile', { user })
-}
+  const userUpdated = await User.findByIdAndUpdate(req.session.userId);
+  res.render("user_profile", { user });
+};
