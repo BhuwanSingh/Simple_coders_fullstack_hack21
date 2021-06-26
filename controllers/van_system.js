520d2800102df005e547bc208e220b23c02a24e0
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-26 21:50:06 +0530

    van system inpliments

--- controllers/van_system.js
+++ controllers/van_system.js
@@ -2,5 +2,6 @@ const Vans = require("../models/vans");
 
 module.exports = async (req, res) => {
   const van_data = await Vans.find({});
-  res.render("van_system", van_data);
+  console.log(van_data)
+  res.render("van_system", {van_data});
 };
