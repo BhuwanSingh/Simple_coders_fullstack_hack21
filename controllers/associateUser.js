3c3b456e53b81a1c4ede4329378ad4c7f2bf5472
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-26 18:29:32 +0530

    get center details

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -1,7 +1,10 @@
 const User = require('./../models/User')
+const Center = require('./../models/center')
 
 module.exports = async (req, res) => {
   console.log(req.session.userId)
   const user = await User.findById(req.session.userId)
+  const center = await Center.findById(req.body.slots)
+  
   res.render('user_profile', { user })
 }
