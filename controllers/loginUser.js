beed2caa325a5a6e1170b906a42e2e6208cea356
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-26 20:01:34 +0530

    login is working now

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,21 +1,36 @@
-const User = require('../models/User')
-module.exports = (req, res) => {
-  const { aadhar, secret_code } = req.body
-  // find one is to be done on book
-  User.findOne({ aadhar }, (error, user) => {
-    console.log(user.secret_code)
-    console.log(secret_code)
-    if (user) {
-      if (user.secret_code == secret_code) {
-        req.session.userID = user._id
-        console.log('User logged in:', aadhar)
-        // res.redirect("/user/profile");
+const Book = require("../models/book");
 
-        // object to be passed is book 
-        res.render('user_profile', { user })
-      }
-    } else {
-      res.redirect('/login')
+// module.exports = (req, res) => {
+//   const { aadhar, secret_code } = req.body;
+//   // find one is to be done on book
+//   Book.findOne({ aadhar }, (error, book) => {
+//     console.log(book.secret_code);
+//     console.log(secret_code);
+//     if (book) {
+//       if (book.secret_code == secret_code) {
+//         // req.session.userID = user._id;
+//         console.log("User logged in:", aadhar);
+//         // res.redirect("/user/profile");
+
+//         // object to be passed is book
+//         res.render("user_profile", { Book });
+//       }
+//     } else {
+//       res.redirect("/login");
+//     }
+//   });
+// };
+
+module.exports = async (req, res) => {
+  const { aadhar, secret_code } = req.body;
+  const book = await Book.findOne({ aadhar });
+
+  if (book) {
+    if (book.secret_code == secret_code) {
+      console.log("user logged in");
+      res.render("user_profile", { book });
     }
-  })
-}
+  } else {
+    res.redirect("/login");
+  }
+};
