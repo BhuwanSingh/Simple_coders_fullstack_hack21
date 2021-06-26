31200806bf48f17dc7b963ba97d7f6238a81c8ae
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-26 19:17:40 +0530

    Update associateUser.js

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -1,15 +1,26 @@
-const User = require("./../models/User");
-const Center = require("./../models/center");
-const Book = require("../models/book")
+const User = require('./../models/User')
+const Center = require('./../models/center')
+const Book = require('../models/book')
 
 module.exports = async (req, res) => {
-  console.log(req.session.userId);
-  const user = await User.findById(req.session.userId);
-  const center = await Center.findById(req.body.slots);
+  console.log(req.session.userId)
+  const user = await User.findById(req.session.userId)
+  const center = await Center.findById(req.body.slots)
 
-  const book = await Book.create( 
-    
-  )
+  const num = await Book.countDocuments({})
+  console.log(num)
+
+  const book = await Book.create({
+    aadhar: user.aadhar,
+    age: user.age,
+    city: user.city,
+    pwd: user.pwd,
+    secret_code: user.secret_code,
+    dose: user.dose + 1,
+    center_name: center.name,
+    center_address: center.address,
+    token: 1,
+  })
 
   res.render('user_profile', { user })
 }
