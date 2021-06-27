8e19f4c27e3b54049725c63eb08f35a5d9b499f8
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-27 08:15:57 +0530

    certificate linked

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,19 +1,27 @@
-const Book = require("../models/book");
-const Center = require("../models/center")
+const Book = require('../models/book')
+const Center = require('../models/center')
+const callAPI = require('./../middleware/certificate')
 
 module.exports = async (req, res) => {
-  const { aadhar, secret_code } = req.body;
-  const book = await Book.findOne({ aadhar });
+  const { aadhar, secret_code } = req.body
+  const book = await Book.findOne({ aadhar })
 
   if (book) {
     if (book.secret_code == secret_code) {
-      const center = await Center.findById(book.center_id);
-
-      console.log("user logged in");
-      res.render("user_profile", { book , c_token: center.current_token});
+      const center = await Center.findById(book.center_id)
+      const cert = await callPI(book)
+      console.log('user logged in')
+      res.render('user_profile', {
+        book,
+        c_token: center.current_token,
+        cert: cert.response,
+      })
     }
   } else {
     // res.redirect("/login");
-    res.render("user_login" , {errors : "Please enter the Aadhar and secret code you've registered your vaccine with."})
+    res.render('user_login', {
+      errors:
+        "Please enter the Aadhar and secret code you've registered your vaccine with.",
+    })
   }
-};
+}
