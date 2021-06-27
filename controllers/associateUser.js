73384e21026fc2f132d5ebb748701877cee3c097
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-27 10:08:22 +0530

    secret code and routes back

--- controllers/associateUser.js
+++ controllers/associateUser.js
@@ -3,33 +3,40 @@ const Center = require('./../models/center')
 const Book = require('../models/book')
 
 module.exports = async (req, res) => {
-  console.log(req.session.userId)
-  const user = await User.findById(req.session.userId)
-  const center = await Center.findById(req.body.slots)
-
-  const book = await Book.findOne({ user_id: user._id })
-  if (book) {
-    var cert = ''
-    res.render('user_profile', { book, c_token: center.current_token, cert })
+  if (req.session.userId == undefined) {
+    return res.redirect('/sign_up')
   } else {
-    const num = await Book.countDocuments({ center_id: center._id })
-    console.log(num)
+    console.log(req.session.userId)
+    const user = await User.findById(req.session.userId)
+    const center = await Center.findById(req.body.slots)
 
-    const book = await Book.create({
-      aadhar: user.aadhar,
-      age: user.age,
-      city: user.city,
-      user_id: user._id,
-      pwd: user.pwd,
-      secret_code: user.secret_code,
-      dose: user.dose + 1,
-      center_id: center._id,
-      center_name: center.name,
-      center_address: center.address,
-      token: num + 1,
-      vaccine: center.vaccine,
-    })
-    var cert = ''
-    res.render('user_profile', { book, c_token: center.current_token, cert })
+    const book = await Book.findOne({ user_id: user._id })
+    if (book) {
+      var cert = ''
+      res.render('user_profile', { book, c_token: center.current_token, cert })
+    } else {
+      const num = await Book.countDocuments({ center_id: center._id })
+      console.log(num)
+
+      const book = await Book.create({
+        aadhar: user.aadhar,
+        age: user.age,
+        city: user.city,
+        user_id: user._id,
+        pwd: user.pwd,
+        secret_code: user.secret_code,
+        dose: user.dose + 1,
+        center_id: center._id,
+        center_name: center.name,
+        center_address: center.address,
+        token: num + 1,
+        vaccine: center.vaccine,
+      })
+      var cert = ''
+      const centerOld = await Center.findByIdAndUpdate(center._id, {
+        slots: center.slots - 1,
+      })
+      res.render('user_profile', { book, c_token: center.current_token, cert })
+    }
   }
 }
--- controllers/associateVan.js
+++ controllers/associateVan.js
@@ -1,29 +1,34 @@
-const User = require("./../models/User");
-const Van = require("./../models/vans");
-const Book = require("../models/book");
+const User = require('./../models/User')
+const Van = require('./../models/vans')
+const Book = require('../models/book')
 
 module.exports = async (req, res) => {
-  console.log(req.session.userId);
-  const user = await User.findById(req.session.userId);
-  const van = await Van.findById(req.body.slots);
+  if (req.session.userId == undefined) {
+    return res.redirect('/sign_up')
+  } else {
+    console.log(req.session.userId)
+    const user = await User.findById(req.session.userId)
+    const van = await Van.findById(req.body.slots)
 
-  const num = await Book.countDocuments({ center_id: van._id });
-  console.log(num);
+    const num = await Book.countDocuments({ center_id: van._id })
+    console.log(num)
 
-  const book = await Book.create({
-    aadhar: user.aadhar,
-    age: user.age,
-    city: user.city,
-    user_id: user._id,
-    pwd: user.pwd,
-    secret_code: user.secret_code,
-    dose: user.dose + 1,
-    center_id: van._id,
-    center_name: van.name,
-    center_address: van.address,
-    token: num + 1,
-    vaccine: van.vaccine,
-  });
-  var cert = "";
-  res.render("user_profile", { book, c_token: van.current_token, cert });
-};
+    const book = await Book.create({
+      aadhar: user.aadhar,
+      age: user.age,
+      city: user.city,
+      user_id: user._id,
+      pwd: user.pwd,
+      secret_code: user.secret_code,
+      dose: user.dose + 1,
+      center_id: van._id,
+      center_name: van.name,
+      center_address: van.address,
+      token: num + 1,
+      vaccine: van.vaccine,
+    })
+    var cert = ''
+
+    res.render('user_profile', { book, c_token: van.current_token, cert })
+  }
+}
