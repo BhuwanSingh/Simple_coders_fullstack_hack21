3a845b4f92a7abb3f97ad1dc1f09c5b7c5f812cc
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-27 10:48:31 +0530

    body-parser removed

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+// const bodyParser = require('body-parser')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 const dotenv = require('dotenv')
 
