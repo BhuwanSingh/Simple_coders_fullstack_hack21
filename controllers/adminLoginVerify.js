cc20c8d507b71315bdf4123c5349900b912199a3
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-27 11:35:12 +0530

    data update and formatting

--- controllers/adminLoginVerify.js
+++ controllers/adminLoginVerify.js
@@ -1,37 +1,18 @@
-const Admin = require("../models/admin");
-
-
-// module.exports = (req, res) => {
-//   const { aadhar, secret_code } = req.body;
-//   admin.findOne({ aadhar }, (error, admin) => {
-//     console.log("test filler123");
-//     console.log(admin.secret_code);
-//     console.log("test filler");
-//     console.log(secret_code);
-//     if (admin) {
-//       if (admin.secret_code == secret_code) {
-//         // req.session.userID = admin._id;
-//         console.log("admin logged in:", aadhar);
-//         res.redirect("/adminPage/verified");
-//       }
-//     } else {
-//       redirect("/");
-//     }
-//     console.log(error);
-//   });
-// };
+const Admin = require('../models/admin')
 
 module.exports = async (req, res) => {
-  const { aadhar, secret_code } = req.body;
+  const { aadhar, secret_code } = req.body
 
   const worker = await Admin.findOne({ aadhar })
-    if (worker && aadhar != null && secret_code != null ) {
-      if (worker.secret_code == secret_code) {
-        // req.session.userID = admin._id;
-        console.log("admin logged in:", aadhar);
-        res.redirect("/adminPage/verified");
-      }
-    } else {
-      res.render("admin_login" , {message : "Enter a valid Health Worker ID and Government Provided Code."})
+  if (worker && aadhar != null && secret_code != null) {
+    if (worker.secret_code == secret_code) {
+      // req.session.userID = admin._id;
+      console.log('admin logged in:', aadhar)
+      res.redirect('/adminPage/verified')
     }
-};
+  } else {
+    res.render('admin_login', {
+      message: 'Enter a valid Health Worker ID and Government Provided Code.',
+    })
+  }
+}
--- controllers/vaccineVerify.js
+++ controllers/vaccineVerify.js
@@ -1,24 +1,28 @@
-const Book = require("../models/book");
-const Center = require("../models/center");
+const Book = require('../models/book')
+const Center = require('../models/center')
 
 module.exports = (req, res) => {
-  const { aadhar, secret_code } = req.body;
+  const { aadhar, secret_code } = req.body
   Book.findOne({ aadhar }, (error, user) => {
     if (user) {
-      console.log(secret_code);
-      console.log(user.secret_code);
+      console.log(secret_code)
+      console.log(user.secret_code)
       if (user.secret_code == secret_code) {
-        console.log("this is verified");
-        Center.findByIdAndUpdate(user.center_id, {
-          current_token: user.token,
-        } , (error , center) => {
-          res.redirect("/adminPage/userverified");
-        });
+        console.log('this is verified')
+        Center.findByIdAndUpdate(
+          user.center_id,
+          {
+            current_token: user.token + 1,
+          },
+          (error, center) => {
+            res.redirect('/adminPage/userverified')
+          }
+        )
       } else {
-        res.redirect("/adminPage/usernotverified");
+        res.redirect('/adminPage/usernotverified')
       }
     } else {
-      res.redirect("/adminPage/usernotverified");
+      res.redirect('/adminPage/usernotverified')
     }
-  });
-};
+  })
+}
