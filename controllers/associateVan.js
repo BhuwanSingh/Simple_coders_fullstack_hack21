049bc2d2767855c019df8d10530e4b6b937d7f90
pratyushgguptaa <pratyushgguptaa@gmail.com>
2021-06-27 12:11:06 +0530

    Update associateVan.js

--- controllers/associateVan.js
+++ controllers/associateVan.js
@@ -10,25 +10,33 @@ module.exports = async (req, res) => {
     const user = await User.findById(req.session.userId)
     const van = await Van.findById(req.body.slots)
 
-    const num = await Book.countDocuments({ center_id: van._id })
-    console.log(num)
+    const book = await Book.findOne({ user_id: user._id })
+    if (book) {
+      var cert = ''
+      res.render('user_profile', { book, c_token: van.current_token, cert })
+    } else {
+      const num = await Book.countDocuments({ center_id: van._id })
+      console.log(num)
 
-    const book = await Book.create({
-      aadhar: user.aadhar,
-      age: user.age,
-      city: user.city,
-      user_id: user._id,
-      pwd: user.pwd,
-      secret_code: user.secret_code,
-      dose: user.dose + 1,
-      center_id: van._id,
-      center_name: van.name,
-      center_address: van.address,
-      token: num + 1,
-      vaccine: van.vaccine,
-    })
-    var cert = ''
-
-    res.render('user_profile', { book, c_token: van.current_token, cert })
+      const book = await Book.create({
+        aadhar: user.aadhar,
+        age: user.age,
+        city: user.city,
+        user_id: user._id,
+        pwd: user.pwd,
+        secret_code: user.secret_code,
+        dose: user.dose + 1,
+        center_id: van._id,
+        center_name: van.name,
+        center_address: van.address,
+        token: num + 1,
+        vaccine: van.vaccine,
+      })
+      var cert = ''
+      const vanOld = await Van.findByIdAndUpdate(van._id, {
+        slots: van.slots - 1,
+      })
+      res.render('user_profile', { book, c_token: van.current_token, cert })
+    }
   }
 }
